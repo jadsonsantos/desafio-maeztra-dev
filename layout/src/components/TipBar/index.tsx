3d81db25a2d2bd65ad14932db04b1492301a9a1c
Jadson Santos <jadsondossantos5@gmail.com>
2023-12-05 20:52:49 -0300

    creates tipbar

---
+++ layout/src/components/TipBar/index.tsx
@@ -0,0 +1,13 @@
+import styles from './TipBar.module.scss'
+
+const TipBar = () => {
+  return (
+    <section className={styles.tipbar}>
+      <div className="container">
+        Acompanhe as melhores promoções disponíveis aqui na Maeztra.
+      </div>
+    </section>
+  )
+}
+
+export default TipBar
